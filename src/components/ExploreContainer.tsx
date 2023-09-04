231f12b5e30596ac26d7157c6b73e876cb7f98be
AriansyahAryo <ariansyah.aryo.prasetio@mhs.politala.ac.id>
2023-09-04 08:16:38 +0700

    MyBackup

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel, IonList, IonThumbnail, IonAccordion, IonAccordionGroup } from '@ionic/react';
 
 function Example() {
   return (
@@ -47,6 +47,76 @@ function Example() {
         </IonCardHeader>
 
         <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+      </IonCard>
+      <IonAccordionGroup>
+        <IonAccordion value="first">
+          <IonItem slot="header" color="light">
+            <IonLabel>First Accordion</IonLabel>
+          </IonItem>
+          <div className="ion-padding" slot="content">
+            <IonCard color="primary">
+              <IonCardHeader>
+                <IonCardTitle>Card Title</IonCardTitle>
+                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+              </IonCardHeader>
+
+              <IonCardContent>Card Content</IonCardContent>
+            </IonCard>
+          </div>
+        </IonAccordion>
+        <IonAccordion value="second">
+          <IonItem slot="header" color="light">
+            <IonLabel>Second Accordion</IonLabel>
+          </IonItem>
+          <div className="ion-padding" slot="content">
+            Second Content
+          </div>
+        </IonAccordion>
+        <IonAccordion value="third">
+          <IonItem slot="header" color="light">
+            <IonLabel>Third Accordion</IonLabel>
+          </IonItem>
+          <div className="ion-padding" slot="content">
+            Third Content
+          </div>
+        </IonAccordion>
+      </IonAccordionGroup>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonList>
+            <IonItem>
+              <IonThumbnail slot="start">
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+              </IonThumbnail>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+
+            <IonItem>
+              <IonThumbnail slot="start">
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+              </IonThumbnail>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+
+            <IonItem>
+              <IonThumbnail slot="start">
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+              </IonThumbnail>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+
+            <IonItem lines="none">
+              <IonThumbnail slot="start">
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+              </IonThumbnail>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonCardContent>
       </IonCard>
     </>
   );
--- src/components/isitab2.tsx
+++ src/components/isitab2.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg } from '@ionic/react';
 
 function Example() {
   return (
-    <IonCard>
-      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-      <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-      </IonCardHeader>
+    <>
+      <IonCard>
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
 
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-    </IonCard>
+        <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+      </IonCard>
+      <IonImg src="https://docs-demo.ionic.io/assets/madison.jpg" alt="The Wisconsin State Capitol building in Madison, WI at night"></IonImg>
+      <IonImg
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdevilchan.com%2Ffocalors-wallpapers-c30059&psig=AOvVaw3yIPljAykYxlsZLQ1Mrr-n&ust=1693876460584000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCPirkNnjj4EDFQAAAAAdAAAAABAD"
+        alt="The Wisconsin State Capitol building in Madison, WI at night"
+      ></IonImg>
+    </>
   );
 }
 export default Example;
